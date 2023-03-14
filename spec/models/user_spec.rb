df18823dbaa2996596a02d5dde42182026f74b47
Antonio Paulino <tonnyarauujo@gmail.com>
2023-03-13 22:33:24 -0300

    add user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User do
+  subject { described_class.create!(name: 'John Doe', email: 'email@test.com') }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email) }
+end
