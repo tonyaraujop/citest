0a5bb0cdb58623479694889c893b15c8a5e8313e
Antonio Paulino <tonnyarauujo@gmail.com>
2023-03-24 12:33:01 -0300

    add describe block to user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,9 @@ require 'rails_helper'
 RSpec.describe User do
   subject { described_class.create!(name: 'John Doe', email: 'email@test.com') }
 
-  it { is_expected.to validate_presence_of(:name) }
-  it { is_expected.to validate_presence_of(:email) }
-  it { is_expected.to validate_uniqueness_of(:email) }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+  end
 end
